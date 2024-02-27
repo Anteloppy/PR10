431fbb227941eea2f643b8cfe3597d62e08b0db8
Anteloppy <polsonya04@gmail.com>
2024-02-28 01:08:15 +0700

    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,6 +28,7 @@ namespace pr10
             InitializeComponent();
             LRole.Content = "Guest";
             LFIO.Content = "";
+            LoadData("select P.id, P.vendor_code, P.name, U.name as unit, P.price, A.size as max_disc, R.name as provider, M.name as manufacturer, C.name as category, I.discaunt as cur_disc, P.quantity_in_stock, P.description, P.picture from Products as P join Units as U on P.unit = U.id join Maximum_discaunts as A on P.max_disc = A.id join Providers as R on P.provider = R.id join Manufacturers as M on P.manufacturer = M.id join Categories as C on P.category = C.id join Current_discaunts as I on P.cur_disc = I.id;");
         }
         public MainWindow(string user)
         {
@@ -65,9 +68,41 @@ namespace pr10
             };
         }
 
+
+        private static string connectionString = "server=localhost; port=3306; database=PR10; user=root; password=Nimda123;";
         public void LoadData(string sql)
         {
+            List<Product> crops = new List<Product>();
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    //("select number, product, quantity, date_order, date_delivery, pickup_point, user, code, status from Orders as O join Products as P on O.Products = P.id join Pickup_points as I on O.Pickup_point = I.id join Users as U on O.User = U.id join Statuses as S on O.Status = S.id", conn);
 
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Product record = new Product();
+                        record.id = reader.GetInt32("id");
+                        record.vendor_code = reader.GetString("vendor_code");
+                        record.name = reader.GetString("name");
+                        record.unit = reader.GetString("unit");
+                        record.price = reader.GetDecimal("price");
+                        record.max_disc = reader.GetInt32("max_disc");
+                        record.provider = reader.GetString("provider");
+                        record.manufacturer = reader.GetString("manufacturer");
+                        record.category = reader.GetString("category");
+                        record.cur_disc = reader.GetInt32("cur_disc");
+                        record.quantity_in_stock = reader.GetInt32("quantity_in_stock");
+                        record.description = reader.GetString("description");
+                        record.picture = reader.GetString("picture");
+
+                        crops.Add(record);
+                    }
+                }
+            }
+            DGProduct.ItemsSource = crops;
         }
 
         private void Exit_MouseDown(object sender, MouseButtonEventArgs e)
---
+++ Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr10
+{
+    internal class Product
+    {
+        public int id { get; set; }
+        public string vendor_code { get; set; }
+        public string name { get; set; }
+        public string unit { get; set; }
+        public decimal price { get; set; }
+        public int max_disc { get; set; }
+        public string provider { get; set; }
+        public string manufacturer { get; set; }
+        public string category { get; set; }
+        public int cur_disc { get; set; }
+        public int quantity_in_stock { get; set; }
+        public string description { get; set; }
+        public string picture { get; set; }
+    }
+}

e7d16a499c4fa968c36b362cc7800eee11f47662
Anteloppy <polsonya04@gmail.com>
2024-02-27 12:41:49 +0700

    Добавьте файлы проекта.

---
+++ EnterWindow.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pr10
+{
+    /// <summary>
+    /// Логика взаимодействия для EnterWindow.xaml
+    /// </summary>
+    public partial class EnterWindow : Window
+    {
+        bool chc = true;
+        public EnterWindow()
+        {
+            InitializeComponent();
+            this.Height = 250;
+            GridLengthConverter gridLengthConverter = new GridLengthConverter();
+            LogWinRow3.Height = (GridLength)gridLengthConverter.ConvertFrom("0*");
+            SPCapcha.Visibility = Visibility.Hidden;
+        }
+
+        //добавить пользователя в скобки
+        public void EnterToSystem(string user)
+        {
+            MainWindow mainWindow = new MainWindow(user);
+            mainWindow.Show();
+            this.Close();
+        }
+
+        public void CapchaGenerate()
+        {
+            Random r = new Random();
+        }
+
+        public void CapchaShow()
+        {
+            MessageBox.Show("Login or password are not correct");
+            chc = false;
+            this.Height = 400;
+            GridLengthConverter gridLengthConverter = new GridLengthConverter();
+            LogWinRow3.Height = (GridLength)gridLengthConverter.ConvertFrom("4*");
+            SPCapcha.Visibility = Visibility.Visible;
+            string capcha = Convert.ToString(LCapcha1.Content) + Convert.ToString(LCapcha2.Content) + Convert.ToString(LCapcha3.Content) + Convert.ToString(LCapcha4.Content);
+            //text = text.Substring(2);
+        }
+
+        public void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            if (chc == true)
+            switch (TBLogin.Text)
+            {
+                case "8lf0g@yandex.ru":
+                    if (TBPassword.Text == "2L6KZG")
+                        EnterToSystem("8lf0g@yandex.ru");
+                    else CapchaShow();
+                    break;
+                case "1zx8@yandex.ru":
+                    if (TBPassword.Text == "uzWC67")
+                        EnterToSystem("1zx8@yandex.ru");
+                    else CapchaShow();
+                    break;
+                case "x@mail.ru":
+                    if (TBPassword.Text == "8ntwUp")
+                        EnterToSystem("x@mail.ru");
+                    else CapchaShow();
+                    break;
+                case "34d@gmail.com":
+                    if (TBPassword.Text == "YOyhfR")
+                        EnterToSystem("34d@gmail.com");
+                    else CapchaShow();
+                    break;
+                case "pxacl@mail.ru":
+                    if (TBPassword.Text == "RSbvHv")
+                        EnterToSystem("pxacl@mail.ru");
+                    else CapchaShow();
+                    break;
+                case "7o1@gmail.com":
+                    if (TBPassword.Text == "rwVDh9")
+                        EnterToSystem("7o1@gmail.com");
+                    else CapchaShow();
+                    break;
+                case "1@gmail.com":
+                    if (TBPassword.Text == "LdNyos")
+                        EnterToSystem("1@gmail.com");
+                    else CapchaShow();
+                    break;
+                case "iut@gmail.com":
+                    if (TBPassword.Text == "gynQMT")
+                        EnterToSystem("iut@gmail.com");
+                    else CapchaShow();
+                    break;
+                case "e3t@outlook.com":
+                    if (TBPassword.Text == "AtnDjr")
+                        EnterToSystem("e3t@outlook.com");
+                    else CapchaShow();
+                    break;
+                case "41clb6o2g@yandex.ru":
+                    if (TBPassword.Text == "JlFRCZ")
+                        EnterToSystem("41clb6o2g@yandex.ru");
+                    else CapchaShow();
+                    break;
+                    default: CapchaShow();
+                    break;
+            }
+        }
+
+        public void CheckCapcha_Click(object sender, RoutedEventArgs e)
+        {
+            string capcha = Convert.ToString(LCapcha1.Content) + Convert.ToString(LCapcha2.Content) + Convert.ToString(LCapcha3.Content) + Convert.ToString(LCapcha4.Content);
+            if (capcha.Equals(TBCapcha.Text))
+            {
+                MessageBox.Show("Capcha has checked");
+                chc = true;
+            }
+            else
+            {
+                MessageBox.Show("Capcha hasn't checked!", "Write capcha again");
+                TBCapcha.Text = "";
+            }
+        }
+
+        private void BGuest_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pr10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LRole.Content = "Guest";
+            LFIO.Content = "";
+        }
+        public MainWindow(string user)
+        {
+            InitializeComponent();
+            //Label role = new Label();
+            //Label fio = new Label();
+            switch (user)
+            {
+                case "8lf0g@yandex.ru": LRole.Content = "Администратор"; LFIO.Content = "Богдан Львович Лавров";
+                    break;
+                case "1zx8@yandex.ru": LRole.Content = "Администратор"; LFIO.Content = "Полина Фёдоровна Смирнова";
+                    break;
+                case "x@mail.ru": LRole.Content = "Администратор"; LFIO.Content = "София Данииловна Полякова";
+                    break;
+                case "34d@gmail.com": LRole.Content = "Менеджер"; LFIO.Content = "Марина Данииловна Чеботарева";
+                    break;
+                case "pxacl@mail.ru": LRole.Content = "Менеджер"; LFIO.Content = "Адам Иванович Ермолов";
+                    break;
+                case "7o1@gmail.com": LRole.Content = "Менеджер"; LFIO.Content = "Андрей Кириллович Васильев";
+                    break;
+                case "1@gmail.com": LRole.Content = "Клиент"; LFIO.Content = "Максим Иванович Маслов";
+                    break;
+                case "iut@gmail.com": LRole.Content = "Клиент"; LFIO.Content = "Михаил Тимурович Симонов";
+                    break;
+                case "e3t@outlook.com": LRole.Content = "Клиент"; LFIO.Content = "Ксения Михайловна Павлова";
+                    break;
+                case "41clb6o2g@yandex.ru": LRole.Content = "Клиент"; LFIO.Content = "Григорий Юрьевич Трифонов";
+                    break;
+            }
+            //LRole = role;
+            //LFIO = fio;
+        }
+
+        public void LoadData(string sql)
+        {
+
+        }
+
+        private void Exit_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            EnterWindow enterWindow = new EnterWindow();
+            enterWindow.Show();
+            this.Close();
+        }
+
+        private void SortA_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void SortZ_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
